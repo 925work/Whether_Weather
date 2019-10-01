bc0c5b1d11b484ad121dfaaf1065f56f7100e0a9
Glen <ultrapancake@gmail.com>
2019-09-30 19:21:59 -0600

    appendResponse function with styling of cards

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,8 +6,153 @@ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipcode
 $.ajax({
     url: queryURL,
     method: "GET"
-  })
-    .then(function(response) {
-      console.log(queryURL);
-      console.log(response);
+})
+    .then(function (response) {
+        console.log(queryURL);
+        console.log(response);
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Glen's code starts here
+
+$(document).on('click', '#submit-button', function () {
+    zipcodeUserInput = $('#data-input').val();
+    // QueryURL stuff from alex in here
+
+    
+    appendResponse();  //function to push information to the HTML
+    $('#data-input').val("");
+})
+
+function appendResponse() {
+    var data = response.data //filler for response
+
+    //for loop for card results
+    for (var i = 0; i < data.length; i++) {
+        
+        var mainDiv = ("<div>");
+        mainDiv.addClass("col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12 m-2");
+        
+        var cardDiv = ("<div>");
+        cardDiv.addClass("card rounded border-success"); //border changes with weather
+        cardDiv.attr({
+            style: "width: 18rem;"
+        })
+        
+        var img = ("<img>");
+        img.addClass("card-img-top");
+        img.attr({
+            'src': data[i].images.original_still.url, //src from results need to change
+            'alt': 'Card image cap'
+        });
+        
+        var cardBody = ("<div>");
+        cardBody.addClass('card-body');
+       
+        var cardTitle = ("<h5>");
+        cardTitle.text("filler event title"); //source response event title 
+        cardTitle.addClass("card-title");
+        
+        var cardText = ("<p>");
+        cardText.text("filler information"); //source info from response
+        cardText.addClass("card-text");
+        
+        var learnMore = ("<a>");
+        learnMore.addClass("btn btn-success"); //source btn color from type of weather
+        learnMore.attr({
+            'href': '#' //the event's URL
+        })
+        learnMore.text('Learn More');
+
+        cardBody.append(learnMore, cardText, cardTitle);
+        cardDiv.append(img, cardBody);
+        mainDiv.append(cardDiv);
+
+        $('#results-div').prepend(mainDiv);
+    }
+}
